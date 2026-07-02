8dcff3557bd58c3c56c41b5355882767acd3e85a
rhen-ren <zerotwo.alanofren@outlook.com>
2026-07-02 22:33:24 +0800

    Add user login functionality and password hashing in user creation

--- service/userservice.py
+++ service/userservice.py
@@ -1,21 +1,33 @@
 from sqlalchemy.orm import Session
 from dependency import get_db
-from schema.user import CreateUser
+from schema.user import CreateUser, LoginUser
+from schema.token import Token,TokenData
 from model.user import User
 from fastapi.exceptions import HTTPException
+from service.authservice import authenticate_user, create_access_token
+from datetime import timedelta
+from pwdlib import PasswordHash
 
-
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+password_hash = PasswordHash.recommended()
 
 def create_user(user: CreateUser, db: Session):
-    #createNew user
-    #TODO: make password into a hash
     try:
         newUser = User(
             name = user.name,
             email = user.email,
-            password = user.password
+            password = password_hash.hash(user.password)
         )
         db.add(newUser)
         db.commit()
     except:
         raise HTTPException(status_code=400)
+    
+def user_login(user: LoginUser, db: Session) -> Token:
+    authenticatedUser = authenticate_user(user.email, user.password, db)
+    if not user:
+        pass
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data = TokenData(email=authenticatedUser.email), expires_delta=access_token_expires)
+
+    return Token(token = access_token)
