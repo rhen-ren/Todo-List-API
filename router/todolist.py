5048921578540b3aff8063aafc3ea24629448bb5
rhen-ren <zerotwo.alanofren@outlook.com>
2026-07-02 13:38:02 +0800

    Implement user registration endpoint to create a new user

--- router/todolist.py
+++ router/todolist.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from schema.user import CreateUser
 from dependency import get_db
+from service.userservice import create_user
 
 
 router = APIRouter()
 
 @router.post("/register")
 def user_register(user: CreateUser, db: Session = Depends(get_db)):
-    pass
+    return create_user(user, db)
