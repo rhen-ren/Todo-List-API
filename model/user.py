329d9ed1f0cb9e8c3a0bae24faa08cd7e702a4e7
rhen-ren <zerotwo.alanofren@outlook.com>
2026-07-02 13:40:26 +0800

    Add unique constraint to email field in User model

--- model/user.py
+++ model/user.py
@@ -9,7 +9,7 @@ class User(Base):
     __tablename__ = "User"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(200), nullable=False)
-    email: Mapped[str] = mapped_column(String(200), nullable=False)
+    email: Mapped[str] = mapped_column(String(200), nullable=False, unique=True)
     password: Mapped[str] = mapped_column(String(200), nullable=False)
 
     notes: Mapped["Note"] = relationship("Note", back_populates="user")
