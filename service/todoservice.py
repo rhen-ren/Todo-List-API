5c7a0fe3eb797829c9b7492ad06e6894db8cac05
rhen-ren <zerotwo.alanofren@outlook.com>
2026-07-03 23:15:23 +0800

    Implement pagination for todos retrieval and enhance delete functionality to return todo details

--- service/todoservice.py
+++ service/todoservice.py
@@ -1,10 +1,21 @@
 from sqlalchemy.orm import Session
-from schema.todo import CreateToDo, GetToDo
+from sqlalchemy import select
+from schema.todo import CreateToDo, GetToDo, GetToDoPaginated
 from service.authservice import get_current_user
 from model.todo import Todo
 from fastapi.exceptions import HTTPException
 from fastapi.responses import JSONResponse
 
+def get_todos(page: int, limit: int, db: Session):
+    todos = db.execute(select(Todo).offset((page - 1)*limit).limit(limit)).scalars().all()
+    total = len(todos)
+    paginatedTodos = GetToDoPaginated(
+        data=[todo for todo in todos],
+        page=page,
+        limit=limit,
+        total=total
+    )
+    return paginatedTodos
 
 def create_todo_item(todo: CreateToDo, token: str, db: Session):
     try:
@@ -56,8 +67,14 @@ def delete_todo_item(todo: CreateToDo, id: int, token: str, db: Session):
         if current_todo.user_id == current_user.id:
             db.delete(current_todo)
             db.commit()
-            return JSONResponse(status_code=204)
+            db.refresh(current_todo)
+            return GetToDo(
+                id=current_todo.id,
+                title=current_todo.title,
+                description=current_todo.description
+            )
         else:
             return {"message" : "forbidden"}
     except:
         raise HTTPException(status_code=401)
+
