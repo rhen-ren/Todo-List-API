86bc4fc894f8e6cfcadf653cbb807bdb00c490a7
rhen-ren <zerotwo.alanofren@outlook.com>
2026-07-03 12:56:48 +0800

    Refactor user registration and login endpoints to improve structure and clarity

--- router/todolist.py
+++ router/todolist.py
@@ -1,12 +1,28 @@
-from fastapi import APIRouter, Depends
+from typing import Annotated
+from fastapi import APIRouter, Depends, Header
 from sqlalchemy.orm import Session
-from schema.user import CreateUser
-from dependency import get_db
-from service.userservice import create_user
+from schema.user import CreateUser, LoginUser
+from schema.todo import CreateToDo
+from dependency import get_db, oauth2_scheme
+from fastapi.security import OAuth2PasswordRequestForm
+from service import userservice, todoservice
 
 
 router = APIRouter()
 
 @router.post("/register")
 def user_register(user: CreateUser, db: Session = Depends(get_db)):
-    return create_user(user, db)
+    return userservice.create_user(user, db)
+
+@router.post("/login")
+def user_login(user: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    login = LoginUser(
+        email=user.username,
+        password=user.password
+    )
+    return userservice.user_login(login, db)
+
+@router.post("/todos")
+def create_todo_item(todo: CreateToDo, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    return todoservice.create_todo_item(todo, token, db)
+
