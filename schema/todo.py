7e1d92b6e64867bb67bc182ec1800116a5a9e8d5
rhen-ren <zerotwo.alanofren@outlook.com>
2026-07-03 23:15:03 +0800

    Refactor todo schema to include model configuration and improve structure

--- schema/todo.py
+++ schema/todo.py
@@ -1,5 +1,4 @@
-from pydantic import BaseModel
-
+from pydantic import BaseModel, ConfigDict
 
 class CreateToDo(BaseModel):
     title: str
@@ -9,3 +8,11 @@ class GetToDo(BaseModel):
     id: int
     title: str
     description: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+class GetToDoPaginated(BaseModel):
+    data: list[GetToDo]
+    page: int
+    limit: int
+    total: int
